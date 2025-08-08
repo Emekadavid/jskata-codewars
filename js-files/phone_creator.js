938f012d0466d7eb81c6194a1f3fa47da3d78363
David Odimegwu <nnaemeka.david@gmail.com>
2025-08-08 06:12:30 +0100

    error in formatted js file

--- js-files/phone_creator.js
+++ js-files/phone_creator.js
@@ -12,7 +12,7 @@ function createPhoneNumber(numbers){
                   numbers.slice(3, 6).join('') + '-' +
                   numbers.slice(-4).join('');
     // return the result
-    return numOutput);
+    return numOutput;
 } 
 
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
