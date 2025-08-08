9d11939eda1d5a01839a8b098d924f5129741b92
David Odimegwu <nnaemeka.david@gmail.com>
2025-08-08 05:21:41 +0100

    Modified the accepted first file

--- phone_creator.js
+++ phone_creator.js
@@ -1,3 +1,11 @@
+// my first codewars kata solution
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+// Task: Write a function that takes an array of 10 integers (between 0 and 9) 
+// and returns a string representing a phone number in the format "(xxx) xxx-xxxx".
+// Example: createPhoneNumber([1,2,3,4,5,6,7,8,9,0]) should return "(123) 456-7890".
+// Note: The input array will always contain exactly 10 integers.
+
 function createPhoneNumber(numbers){
     let numOutput = '('+ numbers.slice(0, 3).join('') + ') ' +
                   numbers.slice(3, 6).join('') + '-' +
