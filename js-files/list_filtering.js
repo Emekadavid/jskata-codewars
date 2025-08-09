40ecff57f7bb96b4bcc22d7a624a3dec128ada5b
David Odimegwu <nnaemeka.david@gmail.com>
2025-08-09 15:33:06 +0100

    finished files of list filtering kata

---
+++ js-files/list_filtering.js
@@ -0,0 +1,27 @@
+/* In this kata you will create a function that takes a list of non-negative 
+integers and strings and returns a new list with the strings filtered out. 
+examples:
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+
+function filter_list(l) {
+  // create the result array
+  let result = [];
+  // iterate through each element in the input array
+  for (let i = 0; i < l.length; i++) {
+  // check if the element is a number
+  if (typeof l[i] === 'number') {
+    // if it is a number, add it to the result array
+    result.push(l[i]);
+    }
+  }
+  return result;
+}
+
+// xample usage
+console.log(filter_list([1, 2, 'a', 'b'])); // [1, 2]
+console.log(filter_list([1, 'a', 'b', 0, 15])); // [1, 0, 15]
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123])); // [1, 2, 123]
